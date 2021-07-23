a12ec0188e1f4ab82f37f90742ac6aa8188b6992
MK <mihaelak08@gmail.com>
2021-07-23 09:56:12 -0700

    formatting

--- portfolio/src/components/Home/index.js
+++ portfolio/src/components/Home/index.js
@@ -2,12 +2,21 @@ import React from "react";
 import "../../index.css";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
 
 function Main() {
   return (
     <body>
       <Row>
-        <Col></Col>
+        <Col>
+          <Card className="about-card">
+            <Card.Body>
+              Hi, I'm Mihaela. Full Stack Web Developer out of Seattle,
+              Washington.
+            </Card.Body>
+          </Card>
+        </Col>
+        {/* <Col></Col> */}
         <Col>
           <img
             src="https://user-images.githubusercontent.com/75143471/120941565-2e7b4c00-c6d8-11eb-87ba-0e409d843ce9.png"
@@ -17,7 +26,6 @@ function Main() {
             className="img-main"
           ></img>{" "}
         </Col>
-        <Col></Col>
       </Row>
     </body>
   );
--- portfolio/src/components/Navbar/index.js
+++ portfolio/src/components/Navbar/index.js
@@ -6,16 +6,14 @@ import Nav from "react-bootstrap/Nav";
 function NavLinks() {
   return (
     <div>
-      <Nav>
-        <Navbar bg="light" variant="light" expland="lg">
-          <Navbar.Brand href="/">Mihaela Kokich</Navbar.Brand>
-          <Nav className="navLinks">
-            <Nav.Link href="/about">About</Nav.Link>
-            <Nav.Link href="/portfolio">Portfolio</Nav.Link>
-            <Nav.Link href="/contact">Contact</Nav.Link>
-          </Nav>
-        </Navbar>
-      </Nav>
+      <Navbar bg="light" variant="light" expland="lg">
+        <Navbar.Brand href="/">Home</Navbar.Brand>
+        <Nav className="navLinks">
+          <Nav.Link href="/about">About</Nav.Link>
+          <Nav.Link href="/portfolio">Portfolio</Nav.Link>
+          <Nav.Link href="/contact">Contact</Nav.Link>
+        </Nav>
+      </Navbar>
     </div>
   );
 }
