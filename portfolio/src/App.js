92480f7357cb3b6c26b1d770e1420594a7265f14
MK <mihaelak08@gmail.com>
2021-07-26 21:01:04 -0700

    condensed pages and more styling

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
-import About from "./pages/About";
-import Portfolio from "./pages/Portfolio";
 import Contact from "./pages/Contact";
 // import "bootstrap/dist/css/bootstrap.min.css";
 // import "./index.css";
@@ -13,10 +11,6 @@ function App() {
       <div>
         <Switch>
           <Route exact path="/" component={Home} />
-
-          <Route exact path="/about" component={About} />
-
-          <Route exact path="/portfolio" component={Portfolio} />
 
           <Route exact path="/contact" component={Contact} />
         </Switch>
