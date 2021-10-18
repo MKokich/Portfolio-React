4f247df6e670562542c15e90b4dea8e7ba7f1cc1
MK <mihaelak08@gmail.com>
2021-10-18 15:44:06 -0700

    fixing route path

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -10,7 +10,7 @@ function App() {
     <Router>
       <div>
         <Switch>
-          <Route exact path="/" component={Home} />
+          <Route path="/" component={Home} />
         </Switch>
       </div>
     </Router>
